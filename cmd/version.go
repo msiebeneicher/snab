39bf8e945564a4e949fa510a28adc90a097cb509
Marc Siebeneicher <marc.siebeneicher@vodafone.com>
2023-03-03 10:49:48 +0100

    feat: add base app structure

---
+++ cmd/version.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	"fmt"
+
+	"snab/pkg/common"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	RootCmd.AddCommand(VersionCmd)
+}
+
+var VersionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number",
+	Long:  `Print the version number`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("snab %s\n", common.Version)
+	},
+}
