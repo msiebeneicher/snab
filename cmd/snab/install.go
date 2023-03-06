47d7da0bbfa2c16c02b625a9fe5fbe1cf9962a73
Marc Siebeneicher <marc.siebeneicher@vodafone.com>
2023-03-06 11:48:26 +0100

    feat(snab-commands): add install and uninstall commands

--- cmd/snab/install.go
+++ cmd/snab/install.go
@@ -2,6 +2,10 @@ package snab
 
 import (
 	"fmt"
+	"os"
+	"snab/pkg/common"
+	"snab/pkg/logger"
+	"snab/pkg/snabfile"
 
 	"github.com/spf13/cobra"
 )
@@ -11,6 +15,24 @@ var InstallCmd = &cobra.Command{
 	Short: "Install snab basket under /usr/local/bin",
 	Long:  `Install snab basket under /usr/local/bin`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Print("TODO: install script under /usr/local/bin/foo")
+		c, _ := snabfile.NewSnabConfigByYaml()
+		scriptPath := fmt.Sprintf("/usr/local/bin/%s", c.Name)
+
+		fileExists, _ := common.IsFile(scriptPath)
+		if fileExists {
+			logger.Infof("snab exec file `%s` already exists\n", scriptPath)
+		} else {
+			script := fmt.Sprintf("#!/usr/bin/env bash\nSNABFILE=%s snab $@", c.Snabfile)
+			logger.WithField("script", script).Debugf("creating snab exec file `%s`\n", scriptPath)
+
+			b := []byte(script)
+			err := os.WriteFile(scriptPath, b, 0755)
+			if err != nil {
+				logger.Error(err)
+				logger.Fatalf("can't create snab exec file `%s`", scriptPath)
+			}
+
+			logger.Infof("snab exec file `%s` successfully created\n", scriptPath)
+		}
 	},
 }
