804b25335ebf411414636ea80ec0e92b2bf204e3
Marc Siebeneicher <marc.siebeneicher@vodafone.com>
2023-03-06 11:48:26 +0100

    feat(snab-commands): add install and uninstall commands

--- cmd/snab/uninstall.go
+++ cmd/snab/uninstall.go
@@ -2,6 +2,10 @@ package snab
 
 import (
 	"fmt"
+	"os"
+	"snab/pkg/common"
+	"snab/pkg/logger"
+	"snab/pkg/snabfile"
 
 	"github.com/spf13/cobra"
 )
@@ -11,6 +15,20 @@ var UninstallCmd = &cobra.Command{
 	Short: "Uninstall snab basket from /usr/local/bin",
 	Long:  `Uninstall snab basket from /usr/local/bin`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Print("TODO: uninstall script under /usr/local/bin/foo")
+		c, _ := snabfile.NewSnabConfigByYaml()
+		scriptPath := fmt.Sprintf("/usr/local/bin/%s", c.Name)
+
+		fileExists, _ := common.IsFile(scriptPath)
+		if !fileExists {
+			logger.Infof("snab exec file `%s` don't exists\n", scriptPath)
+		} else {
+			err := os.Remove(scriptPath)
+			if err != nil {
+				logger.Error(err)
+				logger.Fatalf("can't remove snab exec file `%s`\n", scriptPath)
+			}
+
+			logger.Infof("snab exec file `%s` successfully removed\n", scriptPath)
+		}
 	},
 }
